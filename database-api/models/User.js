77766bb25775b8be0cc91737f81d4aededf9970e
carmenlop <cllopez92@gmail.com>
2021-04-06 12:26:11 -0700

    user set up in db with client and clients pets

--- database-api/models/User.js
+++ database-api/models/User.js
@@ -1,7 +1,58 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const userSchema = new Schema({
+const PetSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    breed: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String,
+        required: true
+    }
+})
+
+const ClientSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    },
+    address: {
+        street: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        zip: {
+            type: String,
+            required: true
+        }
+    },
+    pets: [PetSchema]
+})
+
+const UserSchema = new Schema({
     username: {
         type: String,
         required: true,
@@ -21,7 +72,8 @@ const userSchema = new Schema({
     signup_date: {
         type: Date,
         default: Date.now
-    }
+    },
+    clients: [ClientSchema],
 })
 
 userSchema.set('toJSON', {
@@ -31,6 +83,6 @@ userSchema.set('toJSON', {
     }
 })
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', UserSchema)
 
 module.exports = User;
